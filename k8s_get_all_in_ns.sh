602eed752906fdd1de415fdbb19d88ae47c2492c
stoneshi-yunify <stoneshi@yunify.com>
2020-11-30 14:37:13 +0800

    Update k8s_get_all_in_ns.sh

--- k8s_get_all_in_ns.sh
+++ k8s_get_all_in_ns.sh
@@ -1,1 +1,1 @@
-kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n "$1"
+kubectl api-resources --verbs=list --namespaced -o name | grep -v events | xargs -n 1 kubectl get --show-kind --ignore-not-found -n "$1"
