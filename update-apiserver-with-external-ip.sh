ed623f7a60a75a08bde99a4d6618e4af62f3ca75
stoneshi-yunify <stoneshi@kubesphere.io>
2021-10-29 09:53:10 +0800

    update update-apiserver-with-external-ip.sh
    Signed-off-by: stoneshi-yunify <stoneshi@kubesphere.io>

--- update-apiserver-with-external-ip.sh
+++ update-apiserver-with-external-ip.sh
@@ -16,7 +16,7 @@ done
 
 ip=$1
 
-[[ $(openssl x509 -noout -text -in apiserver.crt | grep -A1 "Subject Alternative Name" | grep -v "Subject Alternative Name" | grep -c "Address:$ip") -gt 0 ]] && echo "External IP $ip already exists in apiserver.crt" && exit 0
+[[ $(openssl x509 -in apiserver.crt -noout -ext "subjectAltName" | grep -c "Address:$ip") -gt 0 ]] && echo "External IP $ip already exists in apiserver.crt" && exit 0
 
 tmpdir=/tmp/_apiserver
 mkdir -p "$tmpdir"
@@ -24,9 +24,16 @@ echo "Backing up apiserver.crt and apiserver.key to $tmpdir ..."
 cp -f apiserver.crt apiserver.key "$tmpdir"
 
 echo "Generating new apiserver.crt and apiserver.key ..."
-openssl genrsa -out apiserver.key 2048
+openssl genrsa -out apiserver.key 4096
 openssl req -new -key apiserver.key -subj "/CN=kube-apiserver," -out apiserver.csr
-echo "subjectAltName = $(openssl x509 -noout -text -in apiserver.crt | grep -A1 "Subject Alternative Name" | grep -v "Subject Alternative Name" | sed 's/ Address//g' | awk '{$1=$1;print}'), IP:$ip" > apiserver.ext
+# generate .ext file, refer to https://www.openssl.org/docs/man1.0.2/man5/x509v3_config.html
+{
+  echo "keyUsage=critical,digitalSignature,keyEncipherment"
+  echo "extendedKeyUsage=serverAuth"
+  echo "basicConstraints=critical,CA:FALSE"
+  echo "authorityKeyIdentifier=keyid"
+  echo "subjectAltName=$(openssl x509 -in apiserver.crt -noout -ext "subjectAltName" | grep -v "Subject Alternative Name" | sed 's/ Address//g' | sed 's/, /,/g' | sed 's/,IP:<invalid>//g' | awk '{$1=$1;print}'),IP:$ip"
+} > apiserver.ext
 openssl x509 -req -in apiserver.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out apiserver.crt -days 365 -extfile apiserver.ext
 
 echo "Generate succeeded"
@@ -34,7 +41,7 @@ echo "Make sure to run this script on all masters"
 
 echo "Do you want to restart all kube-apiserver pods? [yes/no]"
 read -r ans
-[[ $ans != "yes" ]] && exit 1
+[[ $ans != "yes" ]] && exit 0
 
 echo "Restart kube-apiserver ..."
 for apiserver in $(kubectl -n kube-system get pod | awk '{print $1}' | grep "^kube-apiserver")
@@ -44,3 +51,6 @@ done
 
 echo "Please wait a while for the new apiserver pods to start"
 echo "Done"
+
+exit 0
+
